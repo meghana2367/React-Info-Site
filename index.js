2948ffc10f566ca57a1d7f1f9a65a4e7a87a64c7
meghana2367 <maggy.chinta@gmail.com>
2024-06-12 12:48:06 -0500

    Created some basic files and printed Hello React on the screen to check imports are working properly

---
+++ index.js
@@ -0,0 +1,5 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))// regular DOM API
+// ReactDom.render() allows me to render something that looks like html
+// first --> what do I want to render to the screen 
+// second --> where do I want to render it
+// can also get dom nodes with document.querySelector("#root")
