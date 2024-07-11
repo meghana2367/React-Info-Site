6fe3c9be23e78efe439012189315cf4be09b04b2
meghana2367 <maggy.chinta@gmail.com>
2024-07-10 21:25:34 -0500

    Finished up final touches and added in images

--- react-info_site/src/components/Main.js
+++ react-info_site/src/components/Main.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function mainContent(){
+export default function Main(){
     return (
-        <div class = "mainContent">
-            <p id = "main">Fun facts about React</p>
-            <ul>
+        <main class = "mainContent">
+            <h1 className = "main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was orginally created by Jordan Walke</li>
                 <li>Has well over 100k stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
--- react-info_site/src/components/Navbar.js
+++ react-info_site/src/components/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav class = "navBar">
-            <img className = "nav--icon" src = "../images/react-icon-small.png" alt = "hello"/>
+        <nav className = "navBar">
+            <img className = "nav--icon" src = "../images/react-icon-small.png" alt = ""/>
             <h3 className = "nav--logo--text" id = "facts">ReactFacts</h3>
-            <h4 className = "nav--title">React Course - Project 1</h4>
+            <h4 className = "nav--title">I Love React</h4>
         </nav>
     )
 }
